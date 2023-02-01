2caeecf8e5d846d668d96e8cf7f16b650297d101
nicolatoscan <30284178+nicolatoscan@users.noreply.github.com>
2023-02-01 15:01:32 +0000

    Deploying to gh-pages from @ studybuddydev/studybuddy-website@cea1eaa3a7b90abcee46c3a33dc914a832d9e65c 🚀

--- script/main.js
+++ script/main.js
@@ -6,4 +6,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+document.querySelector(".cookie-close").addEventListener("click", (e) => {
+    document.querySelector(".cookie").classList.add("hide");
+    localStorage.setItem("cookie", "true");
+});
+if (!localStorage.getItem("cookie"))
+    document.querySelector(".cookie").classList.remove("hide");
 //# sourceMappingURL=main.js.map
