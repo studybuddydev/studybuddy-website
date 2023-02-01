cea1eaa3a7b90abcee46c3a33dc914a832d9e65c
Nicola Toscan <nicolatoscan99@outlook.it>
2023-02-01 16:00:37 +0100

    cookie

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -6,3 +6,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+document.querySelector(".cookie-close").addEventListener("click", (e) => {
+  document.querySelector(".cookie").classList.add("hide");
+  localStorage.setItem("cookie", "true");
+});
+
+if (!localStorage.getItem("cookie"))
+  document.querySelector(".cookie").classList.remove("hide");
