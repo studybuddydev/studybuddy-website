899755f4e4b42a2fede28eb5c361b15260df5925
nicolatoscan <30284178+nicolatoscan@users.noreply.github.com>
2023-02-03 01:09:46 +0000

    Deploying to gh-pages from @ studybuddydev/studybuddy-website@129f9330a88a0002959ab233c0da2e93e996fc94 🚀

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+// @ts-ignore
+function gtag() { dataLayer.push(arguments); }
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -9,9 +11,28 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 function closeCookie(accepted) {
     localStorage.setItem("cookie", accepted ? "true" : "false");
     document.querySelector(".cookie").classList.add("hide");
+    // @ts-ignore
+    if (accepted)
+        gtag('consent', 'update', { 'ad_storage': 'granted', 'analytics_storage': 'granted' });
 }
 document.querySelector(".cookie-cancel").addEventListener("click", (e) => closeCookie(false));
 document.querySelector(".cookie-accep").addEventListener("click", (e) => closeCookie(true));
-if (!localStorage.getItem("cookie"))
-    document.querySelector(".cookie").classList.remove("hide");
+function main() {
+    // @ts-ignore
+    window.dataLayer = window.dataLayer || [];
+    // @ts-ignore
+    gtag('js', new Date());
+    // @ts-ignore
+    gtag('config', 'G-FR9JDD39YJ');
+    if (localStorage.getItem("cookie") === 'true') {
+        document.querySelector(".cookie").classList.remove("hide");
+        // @ts-ignore
+        gtag('consent', 'default', { 'ad_storage': 'granted', 'analytics_storage': 'granted' });
+    }
+    else {
+        // @ts-ignore
+        gtag('consent', 'default', { 'ad_storage': 'denied', 'analytics_storage': 'denied' });
+    }
+}
+main();
 //# sourceMappingURL=main.js.map
