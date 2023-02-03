40176feeb78a3fb4ac2e981c20f2056a35193e64
Nicola Toscan <nicolatoscan99@outlook.it>
2023-02-03 01:11:48 +0100

    cookie cancel

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -7,10 +7,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-document.querySelector(".cookie-close").addEventListener("click", (e) => {
+function closeCookie(accepted: boolean) {
+  localStorage.setItem("cookie", accepted ? "true" : "false");
   document.querySelector(".cookie").classList.add("hide");
-  localStorage.setItem("cookie", "true");
-});
+}
+
+document.querySelector(".cookie-cancel").addEventListener("click", (e) => closeCookie(false));
+document.querySelector(".cookie-accep").addEventListener("click", (e) => closeCookie(true));
 
 if (!localStorage.getItem("cookie"))
   document.querySelector(".cookie").classList.remove("hide");
