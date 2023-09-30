62b35fd752dc2eb266e7f3880887171977504beb
alessiogandelli <alessiogandelli99@gmail.com>
2023-09-30 16:51:23 +0200

    again hotjar

--- src/scripts/hotjar.ts
+++ src/scripts/hotjar.ts
@@ -1,10 +1,9 @@
-(function(h: any, o: Document, t: string, j: string, a: any, r: any){
-    h.hj = h.hj || function(){(h.hj.q = h.hj.q || []).push(arguments)};
-    h._hjSettings = {hjid: 3675434, hjsv: 6};
-    a = o.getElementsByTagName('head')[0];
-    r = o.createElement('script');
-    r.async = 1;
-    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:3675434,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
     console.log('hotzarro');
     a.appendChild(r);
-})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
